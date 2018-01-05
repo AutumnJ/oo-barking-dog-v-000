8c6a1c3a40c1868a30d5aeb227949a4413138887
Autumn Jung <autumnkangas@gmail.com>
2018-01-05 22:42:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
   def name=(dogs_name)
     @this_dogs_name = dogs_name
   end
- 
+
   def name
     @this_dogs_name
   end
@@ -11,5 +11,5 @@ class Dog
   def bark
     puts "woof!"
   end
-  
+
 end
